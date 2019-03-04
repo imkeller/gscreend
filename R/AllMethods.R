eb752a6ced16287757a8952a72a18d60322b46c3
Katharina Imkeller <katharina.imkeller@embl.de>
2019-03-04 17:06:53 +0100

    Fixed problem with generic method, that was causing problems when object not called object.

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -7,35 +7,35 @@ setMethod("normcounts<-", "PoolScreenExp", function(x, value) {
 
 # get the reference counts for LFC calculation
 setMethod("refcounts", "PoolScreenExp", function(x) {
-    se <- object@sgRNAData
+    se <- x@sgRNAData
     # filter to get T0 samples
     assays(se[, se$timepoint == "T0"])$normcounts
 })
 
 # get the reference counts for LFC calculation
 setMethod("samplecounts", "PoolScreenExp", function(x) {
-    se <- object@sgRNAData
+    se <- x@sgRNAData
     # filter to get T0 samples
     # names have to be checked upon input
     assays(se[, se$timepoint == "T1"])$normcounts
 })
 
 setMethod("samplelfc", "PoolScreenExp", function(x) {
-    se <- object@sgRNAData
+    se <- x@sgRNAData
     # filter to get T0 samples
     # names have to be checked upon input
     assays(se[, se$timepoint == "T1"])$lfc
 })
 
 setMethod("samplelfc", "PoolScreenExp", function(x) {
-    se <- object@sgRNAData
+    se <- x@sgRNAData
     # filter to get T0 samples
     # names have to be checked upon input
     assays(se[, se$timepoint == "T1"])$lfc
 })
 
 setMethod("samplepval", "PoolScreenExp", function(x) {
-    se <- object@sgRNAData
+    se <- x@sgRNAData
     # filter to get T0 samples
     # names have to be checked upon input
     assays(se[, se$timepoint == "T1"])$pval
